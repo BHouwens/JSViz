8feb4164e5b53d9e6df683d842ca0e9ae722bceb
BHouwens <zhion360@gmail.com>
2016-03-11 18:08:41 +0200

    Updates to relationship manager

--- services/rel_manager.py
+++ services/rel_manager.py
@@ -12,16 +12,29 @@ class RelationshipManager:
         """
         self.entries = entries
         self.families = []
-        self.find_dependencies()
+        self.find_import_dependencies()
+        print self.families
         
-    def find_dependencies(self):
+    def find_import_dependencies(self):
+        """
+        Find all strictly imported dependencies for each file and create a "family" with them.
         
+        Structure of a family is such:
+        
+        family = {
+            'parent' : parent file for later reference,
+            'externals' : list of "external" imports, such as angular or jquery,
+            'internals' : list of "internal" imports, those created in this specific project
+        }
+        """
         for entry in self.entries:
             children = entry['children']
             
             for details in children:
                 family = {
-                    'parent' : details['file']
+                    'parent' : details['file'],
+                    'externals' : [],
+                    'internals' : []
                 }
                 
                 path = os.path.join(details['path'], details['file'])
@@ -29,6 +42,29 @@ class RelationshipManager:
                 with open(path, 'r') as file:
                     for line in file:
                         if re.search('import', line) is not None or re.search('require', line) is not None:
+                            # if it's an internal import
                             if re.search('.\/', line) is not None:
-                                print line
-                     
+                                if re.search('\"', line) is not None:
+                                    substring = self.capture_substring('"', line)
+                                    family['internals'].append(substring)
+                                elif re.search('\'', line) is not None:
+                                    substring = self.capture_substring('\'', line)
+                                    family['internals'].append(substring)
+                            # if it's an external import    
+                            else:
+                                if re.search('\"', line) is not None:
+                                    family['externals'].append(self.capture_substring('"', line))
+                                elif re.search('\'', line) is not None:
+                                    family['externals'].append(self.capture_substring('\'', line))
+                                
+                    self.families.append(family)
+                    
+                    
+    def capture_substring(self, char, line):
+        """
+        Returns a substring of a string contained between two entries of char
+        """
+        indices = [i for i, letter in enumerate(line) if letter == char]
+        substring = line[indices[0] + 1: indices[1]]
+        
+        return substring
